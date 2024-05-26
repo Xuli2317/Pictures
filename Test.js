1663e881b64f21e008b101cc047ee760441042c0
Kamonchanok Butaka <157267645+Xuli2317@users.noreply.github.com>
2024-05-27 04:45:15 +0700

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,62 @@
+[
+  {
+    "id": "1",
+    "name": "Cody",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/1.jpg",
+    "Age": "6 years",
+    "Weight": "5 Kg.",
+    "Gender": "Male",
+    "Phonenumber": "022587451",
+    "Address": "Loei"
+  },
+  {
+    "id": "2",
+    "name": "Themis",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/2.jpg",
+    "Age": "4 years",
+    "Weight": "4 Kg.",
+    "Gender": "Male",
+    "Phonenumber": "0965412356",
+    "Address": "Bangkok"
+  },
+  {
+    "id": "3",
+    "name": "Aura",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/3.jpg",
+    "Age": "2 years",
+    "Weight": "3 Kg.",
+    "Gender": "Female",
+    "Phonenumber": "0858548632",
+    "Address": "Bangkok"
+  },
+  {
+    "id": "4",
+    "name": "Carmen",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/4.jpg",
+    "Age": "3 years",
+    "Weight": "4 Kg.",
+    "Gender": "Male",
+    "Phonenumber": "0821236959",
+    "Address": "Kalasin"
+  },
+  {
+    "id": "5",
+    "name": "Olinda",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/5.jpg",
+    "Age": "5 years",
+    "Weight": "4 Kg.",
+    "Gender": "Female",
+    "Phonenumber": "083254123",
+    "Address": "Lamphun"
+  },
+  {
+    "id": "6",
+    "name": "Reet",
+    "uri": "https://raw.githubusercontent.com/Xuli2317/Pictures/main/Adult/Pictures/6.png",
+    "Age": "5 years",
+    "Weight": "3 Kg.",
+    "Gender": "Female",
+    "Phonenumber": "0957485645",
+    "Address": "Nan"
+  }
+]
